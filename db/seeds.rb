8b0f89ad340a2bb124b6b771968ccb1cafe6f93b
cheyenne simoleit <cheyenne@simoleit.com>
2021-08-13 16:13:20 +0200

    half done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+require 'json'
+
+#  URI.open("http://tmdb.lewagon.com/movie/top_rated") {|f|
+#     f.each_line {|line| p line}
+# }
+
+movies = JSON.parse(URI.open("http://tmdb.lewagon.com/movie/top_rated").read)['results']
+
+movies.each do |movie|
+  Movie.create(
+    title: movie['title'],
+    overview: movie['overview'],
+    poster_url: "https://image.tmdb.org/t/p/original#{movie["vote_average"]}",
+    rating: movie["vote_average"])
+end
